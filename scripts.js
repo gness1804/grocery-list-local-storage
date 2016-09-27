9ecb8d45fa0f3328eb9d631a0933e5d08f790b29
gness1804 <graham.nessler@gmail.com>
2016-09-27 14:57:48 -0600

    Initial commit: created basic constructor and input fields

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var titleInput = $("#title-input");
+var bodyInput = $("#body-input");
+
+
+$(document).ready(function () {
+
+  function Idea(title, body, id, quality) {
+    this.title = title;
+    this.body = body;
+    this.id = id || Date.now();
+    this.quality = quality || "swill"; 
+  }
+
+}); //end of jQuery body
