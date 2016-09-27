cd409ff6ea05e85e164216301cc2ba69709617b3
gness1804 <graham.nessler@gmail.com>
2016-09-27 15:05:24 -0600

    added submit button and some minimal styling

--- scripts.js
+++ scripts.js
@@ -2,13 +2,18 @@ var titleInput = $("#title-input");
 var bodyInput = $("#body-input");
 
 
+
 $(document).ready(function () {
 
   function Idea(title, body, id, quality) {
     this.title = title;
     this.body = body;
     this.id = id || Date.now();
-    this.quality = quality || "swill"; 
+    this.quality = quality || "swill";
   }
+
+  Idea.prototype.create = function () {
+
+  };
 
 }); //end of jQuery body
